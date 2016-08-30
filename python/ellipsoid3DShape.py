6df2413f0207bf800697c430c4e6f1b1c46bbac9
Abhishek Agrawal <abhishek.agrawal@protonmail.com>
2016-08-29 23:31:28 -0600

    Add directory to story python plotting scripts. Add script to plot a 3D ellipsoid.

---
+++ python/ellipsoid3DShape.py
@@ -0,0 +1,58 @@
+'''
+Copyright (c) 2016 Abhishek Agrawal (abhishek.agrawal@protonmail.com)
+Distributed under the MIT License.
+See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
+'''
+
+# Set up modules and packages
+# I/O
+import csv
+from pprint import pprint
+
+# Numerical
+import numpy as np
+import pandas as pd
+
+# System
+import sys
+import time
+from tqdm import tqdm
+
+print ""
+print "---------------------------------------------------------------------------------"
+print "                                 NAOS                                            "
+print "                                                                                 "
+print "         Copyright (c) 2016, A. Agrawal (abhishek.agrawal@protonmail.com)        "
+print "---------------------------------------------------------------------------------"
+print ""
+
+# Start timer.
+start_time = time.time( )
+
+# Get plotting packages
+import matplotlib
+import matplotlib.colors
+import matplotlib.axes
+import matplotlib.lines as mlines
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+from matplotlib import rcParams
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import axes3d
+
+# Operations
+
+
+# Stop timer
+end_time = time.time( )
+
+# Print elapsed time
+print "Script time: " + str("{:,g}".format(end_time - start_time)) + "s"
+
+print ""
+print "------------------------------------------------------------------"
+print "                         Exited successfully!                     "
+print "------------------------------------------------------------------"
+print ""
