8b775e4ae6d8f1fb1517394b207a62b0d66d6d17
Abhishek Agrawal <abhishek.agrawal@protonmail.com>
2016-09-19 16:03:05 -0600

    add descriptions for the input arguments in the source and header file

--- include/NAOS/executeOrbiterAroundURE.hpp
+++ include/NAOS/executeOrbiterAroundURE.hpp
@@ -16,6 +16,21 @@ namespace naos
  * a uniformly rotating triaxial ellipsoid. The routine is called in main for execution.
  * @sa ellipsoidGravitationalAcceleration.cpp, orbiterEquationsOfMotion.hpp, rk4.hpp
  *
+ * @param[in] alpha                         largest semi axis of the ellipsoid
+ * @param[in] beta                          intermediate semi axis of the ellipsoid
+ * @param[in] gamma                         smallest largest semi axis of the ellipsoid
+ * @param[in] gravParameter                 gravitational parameter of the ellipsoid
+ * @param[in] density                       density of the ellipsoid
+ * @param[in] Wvector                       angular velocity vector for the ellipsoid
+ * @param[in] Wmagnitude                    magnitude of the angular velocity
+ * @param[in] initialVectorIsCartesian      boolean flag to indicate type of initial vector
+ *                                          value is false for kepler elements, and true for
+ *                                          cartesian elements.
+ * @param[in] initialVector                 vector containing the initial state of the orbiter
+ * @param[in] integrationStepSize           step size to be used for integration
+ * @param[in] startTime                     integration start time
+ * @param[in] endTime                       integration end time
+ * @param[in] filePath                      path to csv file where all the results are stored
  */
  const void executeOrbiterAroundURE( const double alpha,
                                      const double beta,
