50a94c3438f752608c9cfceb8fdd816c60004096
Abhishek Agrawal <abhishek.agrawal@protonmail.com>
2016-12-02 16:37:03 -0700

    add script to plot only crashing trajectories

--- python/plotOrbiterTrajectoryURE.py
+++ python/plotOrbiterTrajectoryURE.py
@@ -53,7 +53,7 @@ start_time = time.time( )
 # Read data in csv file. data returned as a panda series.
 
 ## data in body frame
-data = pd.read_csv( '../data/trajectory_for_different_launch_azimuth/regolithTrajectoryAtAzimuth300.csv' )
+data = pd.read_csv( '../data/trajectory_for_different_launch_azimuth/regolithTrajectoryAtAzimuth210.csv' )
 x = data[ 'x' ].values
 y = data[ 'y' ].values
 z = data[ 'z' ].values
