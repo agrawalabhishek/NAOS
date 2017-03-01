a7447bc5a0a39dff099d7dda57261d25a39df236
Abhishek Agrawal <abhishek.agrawal@protonmail.com>
2017-03-01 12:57:49 +0100

    modified script to plot crash map more efficiently

--- python/plotCrashMapAgainstLaunchVelocity.py
+++ python/plotCrashMapAgainstLaunchVelocity.py
@@ -64,13 +64,13 @@ ax2 = plt.subplot( gs[ 1 ] )
 ## Operations
 # Connect to SQLite database.
 try:
-        database = sqlite3.connect("../data/regolith_launched_from_leading_edge/multiple_launch_velocity/phase_0/simulation_time_9_months/leadingEdge.db")
+        # database = sqlite3.connect("../data/regolith_launched_from_leading_edge/multiple_launch_velocity/phase_0/simulation_time_9_months/leadingEdge.db")
+        database = sqlite3.connect( "../data/regolith_launched_from_longest_edge/spherical_asteroid/longestEdge.db" )
 
 except sqlite3.Error, e:
         print "Error %s:" % e.args[0]
         sys.exit(1)
 
-# for angleValue in tqdm(angleArray):
 data1 = pd.read_sql( "SELECT    initial_position_x,                                         \
                                 initial_position_y,                                         \
                                 initial_position_z,                                         \
@@ -80,7 +80,7 @@ data1 = pd.read_sql( "SELECT    initial_position_x,                             
                                 velocity_x,                                                 \
                                 velocity_y,                                                 \
                                 velocity_z,                                                 \
-                                initial_velocity_magnitude,                                 \
+                                ROUND( initial_velocity_magnitude ),                        \
                                 time,                                                       \
                                 crash_flag,                                                 \
                                 ROUND( launch_azimuth )                                     \
@@ -120,7 +120,7 @@ zPositionStart      = data1[ 'init_pos_z' ]
 ## calculate the lat long for starting point
 startRadialDistance = np.sqrt( xPositionStart**2 + yPositionStart**2 + zPositionStart**2 )
 startLongitude = np.arctan2( yPositionStart, xPositionStart ) * 180.0 / np.pi
-startLatitude = np.arcsin( zPositionStart / startRadialDistance ) * 180 / np.pi
+startLatitude = np.arcsin( zPositionStart / startRadialDistance ) * 180.0 / np.pi
 
 ## calculate lat long for end points
 endRadialDistance = np.sqrt( x**2 + y**2 + z**2 )
@@ -133,9 +133,18 @@ ax1.text( startLongitude[0], startLatitude[0], 'start', size=20, zorder=1, color
 
 ## Plot end points on lat long map
 # ax1.scatter( endLongitude, endLatitude, color='red' )
+unique_reimpact_velocities = np.unique( initial_velocity )
+gridsize = len( unique_reimpact_velocities )
+
+# print gridsize
+# print len(initial_velocity)
+# print len(endLatitude)
+# print len(endLongitude)
+# sys.exit( )
+
 hexBinPlot = ax1.hexbin( endLongitude, endLatitude,                                   \
                          C=initial_velocity,                                          \
-                         cmap='jet', gridsize=30 )
+                         cmap='jet', gridsize=gridsize )
 cbar = plt.colorbar( hexBinPlot, cmap='jet', ax=ax1 )
 
 ## format axis and title
@@ -143,34 +152,48 @@ ax1.set_xlabel('longitude [degree]')
 ax1.set_ylabel('latitude [degree]')
 cbar.ax.set_ylabel( 'Regolith launch velocity [m/s]' )
 ax1.set_xlim( -180.0, 180.0 )
-ax1.set_ylim( -90.0, 90.0, 10.0 )
-ax1.set_title( 'Regolith crash map for multiple launch velocities' )
+ax1.set_yticks( np.arange( -90.0, 90.0, 10.0 ) )
+ax1.set_title( 'Qualitative re-impact map' )
 ax1.grid( True )
 
 ## plot the actual data points, scatter points
-ax2.scatter( endLongitude, endLatitude, c=initial_velocity, cmap='jet' )
-cbar = plt.colorbar( hexBinPlot, cmap='jet', ax=ax2 )
+# ax2.scatter( endLongitude, endLatitude, s=5, c=initial_velocity, edgecolors='face', cmap='jet' )
+# cbar = plt.colorbar( hexBinPlot, cmap='jet', ax=ax2 )
 
+unique_reimpact_velocities = np.unique( initial_velocity )
+colors = plt.cm.Vega20( np.linspace( 0, 1, len( unique_reimpact_velocities ) ) )
+
+for index in range( 0, len( unique_reimpact_velocities ) ):
+    current_velocity = unique_reimpact_velocities[ index ]
+    current_velocity_indices = np.where( initial_velocity == current_velocity )
+    current_velocity_indices = current_velocity_indices[ 0 ]
+    plotLatitude = endLatitude[ current_velocity_indices ]
+    plotLongitude = endLongitude[ current_velocity_indices ]
+    ax2.scatter( plotLongitude, plotLatitude, s=5, c=colors[ index ],                   \
+                 edgecolors='face',                                                     \
+                 label='$V_{launch}$ = ' + str( current_velocity ) + ' [m/s]' )
+
+ax2.legend( ).draggable( )
 ax2.set_xlabel('longitude [degree]')
 ax2.set_ylabel('latitude [degree]')
-cbar.ax.set_ylabel( 'Regolith launch velocity [m/s]' )
+# cbar.ax.set_ylabel( 'Regolith launch velocity [m/s]' )
 ax2.set_xlim( -180.0, 180.0 )
-ax2.set_ylim( -90.0, 90.0, 10.0 )
-ax2.set_title( 'Regolith crash map for multiple launch velocities' )
+ax2.set_yticks( np.arange( -90.0, 90.0, 10.0 ) )
+ax2.set_title( 'Actual re-impact locations' )
 ax2.grid( True )
 
-## Show the plot
-# plt.legend( )
-# plt.tight_layout( )
-# plt.grid( )
-plt.show( )
-# plt.savefig('../data/trajectory_for_different_launch_azimuth/trajectoryPlot_'+str(figureIndex+1)+'.png', dpi=300)
+if database:
+    database.close( )
 
 # Stop timer
 end_time = time.time( )
 
 # Print elapsed time
 print "Script time: " + str("{:,g}".format(end_time - start_time)) + "s"
+
+## Show the plot
+plt.suptitle( 'Regolith crash map for multiple launch velocities \n Spherical asteroid case' )
+plt.show( )
 
 print ""
 print "------------------------------------------------------------------"
